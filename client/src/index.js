82864ebd1b541ef2dbda0f6162770b24117e4af3
Noah Krim <nkrim62@gmail.com>
2018-04-13 17:55:22 -0700

    Started bracket, added quad svg

--- client/src/index.js
+++ client/src/index.js
@@ -2,7 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import Bracket from './bracket/Bracket';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+	<div>
+		<App />
+		<Bracket />
+	</div>
+	, document.getElementById('root'));
 registerServiceWorker();
